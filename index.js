41d3ebc35ad0f30c04d5d9f0a117e12514b3cc3f
minhduc150496 <minhduc150496@gmail.com>
2019-04-02 16:35:12 +0700

    fix fonts

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.listen(PORT);
 
 var confessions =  [
   {
-    sender: "Lan",
-    receiver: "Điệp",
-    message: "From Lan with love.",
+    sender: "Anh",
+    receiver: "Em",
+    message: 'Mỗi lần reng chuông ra về là mình cảm thấy rất hào hứng. Không phải là vì được thả về, mà là vì được đứng chờ chung với cậu ấy. Cho mình hỏi bạn có người yêu chưa, hay có đang thích ai không? - Bữa mình thấy một anh đẹp trai đi ngang qua và bị say nắng bởi nụ cười. Hôm bữa lên trường đua Phú Thọ lại thấy anh một lần nữa, cứ như mình có duyên với nhau í. Mong là anh chưa có ai để ý tới. - Gửi Thanh Ngân: "Bệnh tật có thể cướp đi sinh mạng của anh nhưng nó không thể giết chết tình yêu anh dành cho em. Dù có thế nào trái tim anh vẫn mãi mãi trao cho em như lúc đầu. Anh yêu em. Mà sao em lại không hiểu? Thôi thì anh không muốn em làm người yêu anh nữa đâu, anh chỉ muốn em làm con dâu của ba má anh thôi". THPT chuyên Trần Đại Nghĩa - Nghe nói em đó hổ báo lắm. Mình hay "dị ứng" với những kiểu này lắm. Thế mà hôm nay gặp nhỏ ở căn tin thấy yêu luôn rồi. Đep cách đơn giản, không cần son phấn. Anh thích em mặc áo dài lắm, nhỏ ơi. - Cái anh đi học hay mặc áo khoác xanh biển ấy cho em hỏi anh ấy tên gì vậy, học lớp nào được không ạ? Anh đẹp trai, để mái bằng trông đáng yêu lắm. Mình bị kích thích. Đi học ngày nào cũng phải canh me để được nhìn thấy anh.',
     mp3Code: "ZW6A9I0A",
   },
   {
